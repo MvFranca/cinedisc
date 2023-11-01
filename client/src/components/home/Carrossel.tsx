4fe1fadf2b5d7e0d3ee3a20ed40120e7775d7c0a
Marcos Vinicius <viniciusfranca09@hotmail.com>
2023-11-01 11:00:21 -0300

    Adicionei os botões do carrossel

--- client/src/components/home/Carrossel.tsx
+++ client/src/components/home/Carrossel.tsx
@@ -1,6 +1,8 @@
+import IconBxLeftArrow from "../../icons/IconArrowLeft";
+import IconBxRightArrow from "../../icons/IconArrowRight";
 import "../../styles/home/Carrossel.css";
 import CarrosselPrincipal from "./CarrosselPrincipal";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface Teste {
   overview: string;
@@ -13,6 +15,9 @@ const apiKey = import.meta.env.VITE_API_KEY;
 
 const Carrossel = () => {
   const [newMovies, setNewMovies] = useState<Array<Teste>>([]);
+  const carrossel = useRef<HTMLDivElement>(null);
+  const [scroll, setScroll] = useState(0);
+  const [intervalo, setIntervalo] = useState();
 
   async function conteudo(moviesURL: string) {
     const api = await fetch(moviesURL);
@@ -20,24 +25,73 @@ const Carrossel = () => {
     setNewMovies(json.results);
   }
 
+  const avancar = () => {
+
+    const teste = carrossel.current!.scrollWidth;
+
+    
+    if((teste - teste/newMovies.length) <= carrossel.current!.scrollLeft) return carrossel.current!.scrollLeft = 0
+
+    carrossel.current!.scrollLeft += teste / newMovies.length;
+
+    console.log(`Largura: ${teste - teste/newMovies.length}\nPosição Atual: ${carrossel.current!.scrollLeft }`)
+
+  };
+
+  const voltar = () => {
+    const teste = carrossel.current!.scrollWidth;
+
+    carrossel.current!.scrollLeft -= teste / newMovies.length;
+  };
+
   useEffect(() => {
     const urlFull = `${moviesURL}now_playing?${apiKey}&language=pt-BR`;
 
     conteudo(urlFull);
   }, []);
 
+  /*
+  useEffect(() => {
+    let time = 0;
+
+    if (carrossel.current!.scrollLeft == 0) {
+      time = 2000;
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      time = 4000;
+    }
+
+    let teste = setTimeout(() => {
+      setScroll(scroll + 1);
+      console.log(teste);
+      avancar();
+    }, time);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scroll]);
+
+*/
+
   return (
-    <div className="carrossel-principal">
+    <div className="carrossel-principal" ref={carrossel}>
       {newMovies.length === 0 && <p>carregando...</p>}
 
       {newMovies.length > 0 &&
         newMovies.map((filme) => (
-          <CarrosselPrincipal
-            backdrop_path={filme.backdrop_path}
-            title={filme.title}
-            overview={filme.overview}
-          />
+          <section>
+            <CarrosselPrincipal
+              backdrop_path={filme.backdrop_path}
+              title={filme.title}
+              overview={filme.overview}
+            />
+          </section>
         ))}
+      <div className="arrow-left" onClick={voltar}>
+        <IconBxLeftArrow color="#fff" />
+      </div>
+      <div className="arrow-right" onClick={avancar}>
+        <IconBxRightArrow color="#fff" />
+      </div>
     </div>
   );
 };
---
+++ client/src/icons/IconArrowLeft.tsx
@@ -0,0 +1,18 @@
+// icon:bx-left-arrow | Boxicons https://boxicons.com/ | Atisa
+import * as React from "react";
+
+function IconBxLeftArrow(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M18.464 2.114a.998.998 0 00-1.033.063l-13 9a1.003 1.003 0 000 1.645l13 9A1 1 0 0019 21V3a1 1 0 00-.536-.886zM17 19.091L6.757 12 17 4.909v14.182z" />
+    </svg>
+  );
+}
+
+export default IconBxLeftArrow;
